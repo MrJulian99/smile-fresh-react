910ba85410e12c6105c0a8f6bb22972451f8f2cd
santiago <santiagos9511@gmail.com>
2017-06-03 17:20:52 -0500

    citas por mes
    toma las citas del mes, aun no se listan pues no toma el componente

--- src/CitasPorMes.js
+++ src/CitasPorMes.js
@@ -4,16 +4,62 @@ import CitaIndividual from'./CitaIndividual'
 
 export default class CitaPorMes extends Component{
 	render(){
+	
+			let listaCitasAux=[];
 		let listaCitas =[];
 		let final = null;
+		let fecha1=[];
+		let listaCitasMirar =[];
+			let x;
+			let fecha=[];
 		
 
+		function buscar() {
+		
+				
+				if(fecha !== null ){
+			 		x = document.getElementById("myMonth").value;
+			 		fecha=x.split("-");	
+				}
+
+				 listaCitasAux=CitasRegistradas.sort(function(a,b){
+				    a=a.fecha.split("-");
+				    b=b.fecha.split("-");
+				    return new Date(a[0], a[1],a[2] ) - new Date(b[0], b[1], b[2])
+				  })
+
+			
+				listaCitasAux.forEach((cita)=>{
+
+		 			if (cita.id !== final){
+		 			fecha1=cita.fecha.split("-")
+		 			//
+		 			if(fecha1[1]==fecha[1] && fecha1[0]==fecha[0]  ){
+		 				console.log("entro");
+	 				listaCitasMirar.push(cita);
+	 				//listaCitas.push(<CitaIndividual cita={cita} key={cita.id} />);
+	 				listaCitas.push(<tr><td>{cita.fecha}</td>
+					<td>{cita.id} </td></tr>
+					);
+				
 	
+		 			}
+		 			//console.log("citas mes");
+		 			//console.log(listaCitasMirar);
+		 			
+		 		}
+		 		
+		 	
+		 		
+		 		
+		 	})
+
+			}
      
 
 
 
-		 	CitasRegistradas.forEach((cita)=>{
+		 /*	CitasRegistradas.forEach((cita)=>{
 		 		if (cita.fecha !== final){
 		 			listaCitas.push(<CitaIndividual cita={cita} key={cita.id} />);
 		 		}
@@ -21,14 +67,18 @@ export default class CitaPorMes extends Component{
 		 	
 		 		
 		 		
-		 	})
-
+		 	})*/
+				
 		return(
 
 
       <center>
 				<div>
         <h3>Citas</h3>
+        <div>
+           	<input type="month" id="myMonth" />
+       		<button type="button" name="button" onClick={buscar}>Buscar</button> 		
+           </div>
 				<table className= "table1">
 					<thead>
 						<tr>
