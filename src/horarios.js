00922cf1233d327ba0bbee42adda3828b655b17f
Victor Julian <victorjulian24@gmail.com>
2017-06-02 22:50:00 -0500

    Avance  del form para ingreso de horarios

--- src/horarios.js
+++ src/horarios.js
@@ -11,12 +11,21 @@ export default class horario  extends Component{
 			super()
 			this.state = {
 
-				isActive:false
-			}
+				isActive:false,
+				value:'Lunes',
+				valueh:'8:00 am - 8:30 am'
 
+			}
+			this.handleChange = this.handleChange.bind(this);
 	}
 
+	handleChange(event) {
+		const target = event.target;
+			const value = target.value;
+			const name = target.name;
+	    this.setState({[name]: value});
 
+	  }
 
 componentWillMount(){
 
@@ -37,7 +46,10 @@ Modal.setAppElement('body');
 
 	}
 
+	aceptar = () => {
+			alert(this.state.value + '    ' + this.state.valueh )
 
+	}
 
 
 
@@ -94,11 +106,46 @@ Modal.setAppElement('body');
 <button class="button button1" onClick={this.toggleModal} >Nuevo Horario</button>
 <Modal isOpen={this.state.isActive} onRequestClose={this.toggleModal}>
 
-		Hello from modal.
+		<h1>Ingresar Nuevo Horario</h1>
+
+<h3 class="text-left " >Ingrese la siguiente informacion referente al registro de un nuevo horario para un medico:</h3>
 
 
+<h4 class="text-left " >Seleccione un medico:</h4>
 
-<button class="button button1" onClick={this.toggleModal} >close</button>
+<h4 class="text-left " >Seleccione un dia de la semana:</h4>
+<div class="form-group">
+<select class="form-control" name="value" value={this.state.value} onChange={this.handleChange}>
+									<option value="Lunes">Lunes</option>
+									<option value="Martes">Martes</option>
+									<option value="Miercoles">Miercoles</option>
+									<option value="Jueves">Jueves</option>
+									<option value="Viernes">Viernes</option>
+									<option value="Sabado">Sabado</option>
+									<option value="Domingo">Domingo</option>
+								</select>
+</div>
+
+<h4 class="text-left " >Seleccione un horario:</h4>
+<div class="form-group">
+<select class="form-control" name="valueh" value={this.state.valueh} onChange={this.handleChange}>
+									<option >8:00 am - 8:30 am </option>
+									<option >8:30 am - 9:00 am </option>
+									<option >9:00 am - 9:30 am</option>
+									<option >10:00 am - 10:30 am</option>
+									<option >10:30 am - 11:00 am</option>
+									<option >11:00 am - 11:30 am</option>
+									<option >11:30 am - 12:30 am</option>
+								</select>
+</div>
+
+<pre></pre>
+
+<div class="form-group">
+<button class="button button1" onClick={this.toggleModal} >Cerrar</button>
+<button class="button button1" onClick={this.aceptar} >Aceptar</button>
+</div>
+
 
 </Modal>
 
